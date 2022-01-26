5b2717032850f4ba8ebecfc4b8fa0e679613c4e5
Ninh Văn Tuấn Đạt <86716708+Ninh-Dat@users.noreply.github.com>
2022-01-26 08:40:08 +0700

    bai tap

--- bai1/index.php
+++ bai1/index.php
@@ -1,1 +1,11 @@
 <?php
+ function createArr($number)
+ {
+     $arr = [];
+     for ($i = 0; $i < $number; $i++) {
+         $arr[] = rand(1, 100);
+     }
+    return $arr;
+ }
+ echo "<pre>";
+ print_r(createArr(10));
